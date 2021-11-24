66312872f36dfa5d4364af65876cef044a6e275c
Aedanos <santri0303@gmail.com>
2021-11-24 22:22:24 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,9 @@
+import telebot
 
+bot = telebot.TeleBot("2133620366:AAEvwjkQqLG5yxGIwVdpOqe5OsaG01LBD4o")
+
+@bot.message_handler(contact_types=['text'])
+def send_echo(message):
+	bot.reply_to(message, message.text)
+
+bot.polling(none_stop = True)
